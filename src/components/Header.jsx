44181c2711c871921b921929a3d8b5bfdd6539d5
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-02-04 15:58:22 +0900

    一旦全消し

--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import '../App.css';
-
-export function Header() {
-    return (
-        <h1>VALORANT Oekaki Chat</h1>
-    );
-}
--- src/components/Reset.jsx
+++
@@ -1,9 +0,0 @@
-import '../App.css';
-
-export function Reset(props) {
-    return (
-        <div className='buttons'>
-            <button className='actionClicked reset' onClick={props.resetData}>Reset</button>
-        </div>
-    );
-}
