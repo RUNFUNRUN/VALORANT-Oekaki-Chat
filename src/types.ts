c44f1b28484fb5c03d90fb623d0064891f4eaca2
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-02-05 12:46:07 +0900

    fix type

--- src/types.ts
+++ src/types.ts
@@ -5,5 +5,4 @@ export type Resolution = 'fullhd' | 'stretch';
 const heightSchema = z.number().int().min(1).max(13);
 export type Height = z.infer<typeof heightSchema>;
 
-const asciiDataSchema = z.array(z.array(z.boolean()));
-export type AsciiData = z.infer<typeof asciiDataSchema>;
+export type AsciiData = boolean[][];
