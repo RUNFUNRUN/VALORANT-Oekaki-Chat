7610803ff2e529ba8ab769906209f5dc71da8032
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-03-26 22:02:33 +0900

    fix footer

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+export const Footer = () => {
+  return (
+    <footer className='mb-2 mt-auto text-center text-xl font-semibold lg:flex lg:justify-center lg:gap-10'>
+      <p className='hidden lg:block'>&copy; 2022 RUNFUNRUN</p>
+      <p>
+        <Link
+          href='https://twitter.com/GRAPH_fps'
+          className='text-blue-500 hover:underline'
+        >
+          Twitter
+        </Link>
+        <span className='font-light mx-2'>|</span>
+        <Link
+          href='https://github.com/RUNFUNRUN/VALORANT-Oekaki-Chat'
+          className='text-blue-500 hover:underline'
+        >
+          GitHub
+        </Link>
+        <span className='font-light mx-2'>|</span>
+        <Link
+          href='https://www.runfunrun.info'
+          className='text-blue-500 hover:underline'
+        >
+          Portfolio
+        </Link>
+        <span className='font-light mx-2'>|</span>
+        <Link
+          href='https://www.runfunrun.tech'
+          className='text-blue-500 hover:underline'
+        >
+          Blog
+        </Link>
+      </p>
+      <p className='lg:hidden mt-4'>&copy; 2022 RUNFUNRUN</p>
+    </footer>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Lobster } from 'next/font/google';
-import Link from 'next/link';
 import { DonateButton } from './_components/DonateButton';
 import { Header } from './_components/Header';
 import { OekakiChat } from './_components/OekakiChat';
 import { OpinionBox } from './_components/OpinionBox';
 import { Share } from './_components/Share';
+import { Footer } from './_components/Footer';
 
 const lobstar = Lobster({ weight: '400', subsets: ['latin'] });
 
@@ -19,24 +19,7 @@ const Home = () => {
       <Share />
       <OpinionBox />
       <DonateButton />
-      <footer className='mb-2 mt-auto text-center text-xl font-bold'>
-        <p>
-          Made by RUNFUNRUN |{' '}
-          <Link
-            href='https://twitter.com/GRAPH_fps'
-            className='text-blue-500 underline'
-          >
-            Twitter
-          </Link>{' '}
-          |{' '}
-          <Link
-            href='https://github.com/RUNFUNRUN/VALORANT-Oekaki-Chat'
-            className='text-blue-500 underline'
-          >
-            GitHub
-          </Link>
-        </p>
-      </footer>
+      <Footer />
     </main>
   );
 };
