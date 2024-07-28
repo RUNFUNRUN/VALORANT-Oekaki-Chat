a5c6323a3244f49742528259353253912a811e15
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-07-28 16:27:47 +0900

    fix share and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,17 @@
-import { Lobster } from 'next/font/google';
 import { DonateButton } from './_components/donate-button';
 import { Footer } from './_components/footer';
 import { Header } from './_components/header';
-import { Info } from './_components/info';
 import { OekakiChat } from './_components/oekaki-chat';
 import { OpinionBox } from './_components/opinion-box';
 import { Share } from './_components/share';
 
 export const runtime = 'edge';
 
-const lobstar = Lobster({ weight: '400', subsets: ['latin'] });
-
 const Home = () => {
   return (
     <main>
       <Header />
-      <p className={`text-center mt-4 text-2xl ${lobstar.className}`}>
-        &quot;Oekaki&quot; means &quot;drawing&quot; in Japanese.
-      </p>
       <OekakiChat />
-      <Info />
       <Share />
       <OpinionBox />
       <DonateButton />
