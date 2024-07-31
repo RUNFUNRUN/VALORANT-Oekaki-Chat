9ac091309738b1aed2e2e5146dcd776750a94099
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-07-31 23:13:21 +0900

    fix auth arg

--- src/components/auth-components.tsx
+++ src/components/auth-components.tsx
@@ -7,28 +7,27 @@ export const SignIn = ({
   provider,
   ...props
 }: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) => {
-  const handleSignIn = async () => {
-    await signIn(provider, {
-      callbackUrl: '/',
-    });
-  };
   return (
-    <Button {...props} onClick={handleSignIn}>
+    <Button
+      {...props}
+      onClick={async () => {
+        await signIn();
+      }}
+    >
       Sign In
     </Button>
   );
 };
 
 export const SignOut = (props: React.ComponentPropsWithRef<typeof Button>) => {
-  const handleSignOut = async () => {
-    await signOut({ callbackUrl: '/' });
-  };
   return (
     <Button
       variant='ghost'
       className='w-full p-0'
       {...props}
-      onClick={handleSignOut}
+      onClick={async () => {
+        await signOut();
+      }}
     >
       Sign Out
     </Button>
